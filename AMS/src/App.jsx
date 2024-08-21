804a0693439c3fed271e0d997dce7007d7bb8e3a
Arju7500 <an096237@gmail.com>
2024-08-21 22:25:03 +0530

    headerpart update

--- AMS/src/App.jsx
+++ AMS/src/App.jsx
@@ -1,13 +1,13 @@
-import Footer from "./components/Footer"
-import Header from "./components/Header"
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-function App() {
+const App=()=> {
     return (
     <>
     <Header />
     <Footer />
     </>
   )
-}
+};
 
-export default App
+export default App;
--- AMS/src/main.jsx
+++ AMS/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "./Style.css"
-import "./style/Header.css"
-import App from './App'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
